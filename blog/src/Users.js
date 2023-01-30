b95961feb8f88e71abb90ea237d54d870a49fe9d
sagar-ss <sagarsrivastava0302@gmail.com>
2023-01-31 00:31:49 +0530

    using HTML inside of js without help of JSX

--- blog/src/Users.js
+++ blog/src/Users.js
@@ -1,10 +1,6 @@
-
+import React from 'react'
 function User(){
-  return (
-
-    <div>JSX! {10+20}</div>
-
-  )
+  return React.createElement('div', null,React.createElement('h1', null, 'hello beginners in new way'));
 }
 
 export default User;
