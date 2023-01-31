5a3b3f19901e4d9075f13f20157de7d6a92384b0
sagar-ss <sagarsrivastava0302@gmail.com>
2023-01-31 11:30:20 +0530

    back to Normal

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,35 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-// import {useState} from 'react';
-import React,{Component} from 'react'
 
-class App extends Component {
-
-  constructor()
-  {
-    super();
-    this.state= {
-      data:"Sagar"
-    }
-  }
-
-  updateData()
-  {
-    // this.setState({data:Number(0)});
-    this.setState({data:this.state.data+1})
-  }
-  render()
-  {
-
-    return (
-      <div className="App">
-        <h1>{this.state.data}</h1>
-        <button onClick={()=>{this.updateData()}}>Update Data</button>
-      </div>
-    );
-   
-  }
-  
+function App() {
+  return (
+    <div className="App">
+     <h1>hello beginners</h1>
+    </div>
+  );
 }
 
 export default App;
